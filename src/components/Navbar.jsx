5f7a3b9bdcae9f5caa92c666c77e3ef22e3e94a5
Alter2k20 <mishraashutosh98611@gmail.com>
2024-10-21 18:02:50 +0530

    initial

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+
+        <nav>
+            <ul>
+                <li><i>SpaceRails</i>.unitednations</li>
+                <li>Get Tickets</li>
+                <li>Raise a ticket</li>
+                <li>About</li>
+            </ul>
+        </nav>
+
+    )
+}
+
+export default Navbar
---
+++ src/components/locations.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function locations() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default locations
