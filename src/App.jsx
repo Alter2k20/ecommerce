184108bcc43811fd4ace7db9e0432a218fc0722c
Alter2k20 <mishraashutosh98611@gmail.com>
2024-12-15 08:42:59 +0530

    erkjbgke

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import Navbar from "./components/Navbar"
 
 function App() {
-  return <div className=' h-[100vh] w-[100vw] pt-4'>
+  return <div className=' h-[100vh] w-[100vw] pt-4 drop-shadow-[rgba(0,255,0,0.25)]'>
   <Navbar />
   </div>
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom"
 function Navbar() {
     return (
 
-        <nav className=' w-[85vw] m-auto text-white rounded-full'>
+        <nav className=' w-[85vw] m-auto text-white rounded-full bg-white bg-opacity-50 '>
             <ul className="flex justify-around">
                 <li className='italic text-3xl  font-bold'>Lorem, ipsum.</li>
                 <div className="flex justify-around ">
-                <li className="font-bold m-auto p-2 border-l border-r cursor-pointer  duration-700">cart</li>
-                <li className="font-bold m-auto p-2 cursor-pointer  duration-700"><Link to={"/Login"}>Login</Link></li>
-                <li className="font-bold m-auto p-2 border-l border-r cursor-pointer  duration-700">Raise a ticket</li>
+                <li className="font-bold m-auto p-2 border-l border-r cursor-pointer  duration-700 hover:bg-orange-600">cart</li>
+                <li className="font-bold m-auto p-2 cursor-pointer  duration-700 hover:bg-orange-600"><Link to={"/Login"}>Login</Link></li>
+                <li className="font-bold m-auto p-2 border-l border-r cursor-pointer  duration-700 hover:bg-orange-600">Raise a ticket</li>
                 </div>
             </ul>
         </nav>
